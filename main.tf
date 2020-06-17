5bf42c0d1bd9417251924accc30af0d23313f4b3
Colin Wilson <colin.wilson@protonmail.ch>
2020-06-17 16:47:56 +0100

    remove Quad9 nameserver

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "rancher2_cluster" "cluster" {
     }
 
     dns {
-      upstream_nameservers = ["1.1.1.1","9.9.9.9","8.8.8.8"]
+      upstream_nameservers = ["1.1.1.1","8.8.8.8"]
     }
 
     ingress {
